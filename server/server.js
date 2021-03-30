ae4e4860f3fb6e55724794755abad5d7d3de56f6
Andrew-Hany <andrewhany@aucegypt.edu>
2021-03-30 02:21:56 +0200

    backend is almost done

--- server/server.js
+++ server/server.js
@@ -1,11 +1,73 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const bodyParser = require('body-parser')
+const mysql = require('mysql');
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'travelling_agency',
+  port: 3306
+
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+//-----------------------company----------------------//
+app.get("/company", (req, res) => {
+  const sqlget = 'SELECT * FROM travelling_agency.company;'
+  db.query(sqlget, (err, result) => {
+      res.send(result);
+      console.log(result)
+
+  });
+});
+//-----------------------users----------------------//
+app.get("/users", (req, res) => {
+  const sqlget = 'SELECT * FROM travelling_agency.users;'
+  db.query(sqlget, (err, result) => {
+      res.send(result);
+      console.log(result)
+
+  });
+});
+//-----------------------trips----------------------//
+app.get("/trips", (req, res) => {
+  const sqlget = 'SELECT * FROM travelling_agency.trips;'
+  db.query(sqlget, (err, result) => {
+      res.send(result);
+      console.log(result)
+
+  });
+});
+//-----------------------rates----------------------//
+app.get("/rates", (req, res) => {
+  const sqlget = 'SELECT * FROM travelling_agency.rates;'
+  db.query(sqlget, (err, result) => {
+      res.send(result);
+      console.log(result)
+
+  });
+});
+//-----------------------reservation----------------------//
+app.get("/reservation", (req, res) => {
+  const sqlget = 'SELECT * FROM travelling_agency.reservation;'
+  db.query(sqlget, (err, result) => {
+      res.send(result);
+      console.log(result)
+
+  });
+});
+//-----------------------Trips_photos----------------------//
+app.get("/Trips_photos", (req, res) => {
+  const sqlget = 'SELECT * FROM travelling_agency.Trips_photos;'
+  db.query(sqlget, (err, result) => {
+      res.send(result);
+      console.log(result)
+
+  });
+});
+app.listen(8001)
--- travel_buddy/src/App.js
+++ travel_buddy/src/App.js
@@ -1,18 +1,48 @@
+import React, { Component } from 'react';
 import Nav from './components/navbar';
 import Prof from './components/profile';
 import Cards from './components/cards';
 import './App.css';
+import Axios from "axios";
 
-function App() {
-  return (
+
+
+
+
+
+class App extends Component {
+
+state = {
+  Users:[]
+};
+
+componentDidMount() {
+  Axios.get('http://localhost:8001/users').then((response) => {
+    this.setState({ Users: response.data })
+   
+    console.log(this.state.Users)
+
+  })
+};
+
+
+  render() {
+
+    return (
     <div className="App">
       {/* call the components here */}
-      <Nav/>   {/* example  to call, you can pass attributes inside the calls*/}
-      <Prof/>
-      <Cards/>
+      {/* <Nav/>   {/* example  to call, you can pass attributes inside the calls*/}
+      {/* <Prof/> */} 
+      {
+      this.state.Users.map(user=>{
+        return <h1>{user.user_fname} {user.user_lname}</h1>
+      })
+      } 
+      {/* <Cards/> */}
 
     </div>
   );
+  }
 }
 
 export default App;
